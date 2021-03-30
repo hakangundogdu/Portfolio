a4ae678be1679e42cdb72117217b414aceeed1ec
Hakan Gundogdu <hakan.gundogdu@gmail.com>
2021-03-31 01:33:31 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 //import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import { Header } from "./Components/Header";
+import logo from './logo.svg';
+import './App.css';
+import { Header } from './Components/Header';
 
 const App = () => <Header title="Hakan Gundogdu" />;
 
