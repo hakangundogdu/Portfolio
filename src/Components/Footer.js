020ae1aa1003250fa140a30e616aba0d15faadf3
Hakan Gundogdu <hakan.gundogdu@gmail.com>
2021-08-16 00:09:57 +0300

    footer all rights..

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -10,79 +10,84 @@ import link from '../assets/link.png';
 const Footer = () => {
   return (
     <div className="footer" id="contact">
-      <h2 className="footer-title">Contact Me</h2>
-      <div className="footer-container">
-        <div className="contact-icon">
-          <Icon
-            src={mail}
-            alt="mail icon"
-            link="mailto:hakan.gundogdu@gmail.com"
-          />
-          <a
-            href="mailto:hakan.gundogdu@gmail.com"
-            target="_blank"
-            rel="noreferrer"
-          >
-            hakan.gundogdu@gmail.com
-          </a>
-        </div>
-        <div className="contact-icon">
-          <Icon
-            src={linkedin}
-            alt="linkedin icon"
-            link="https://www.linkedin.com/in/hakangundogdu1/"
-          />
-          <a
-            href="https://www.linkedin.com/in/hakangundogdu1/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            linkedin.com/in/hakangundogdu1/
-          </a>
-        </div>
-        <div className="contact-icon">
-          <Icon
-            src={github}
-            alt="github icon"
-            link="https://github.com/hakangundogdu"
-          />
-          <a
-            href="https://github.com/hakangundogdu"
-            target="_blank"
-            rel="noreferrer"
-          >
-            github.com/hakangundogdu
-          </a>
-        </div>
-        <div className="contact-icon">
-          <Icon
-            src={link}
-            alt="link icon"
-            link="https://upwork.com/freelancers/hakan"
-          />
-          <a
-            href="https://upwork.com/freelancers/hakan"
-            target="_blank"
-            rel="noreferrer"
-          >
-            upwork.com/freelancers/hakan
-          </a>
-        </div>
-        <div className="contact-icon">
-          <Icon
-            src={twitter}
-            alt="twitter icon"
-            link="https://twiter.com/hakancode"
-          />
-          <a
-            href="https://twiter.com/hakancode"
-            target="_blank"
-            rel="noreferrer"
-          >
-            twiter.com/hakancode
-          </a>
+      <div className="footer-main">
+        <h2 className="footer-title">Contact Me</h2>
+        <div className="footer-container">
+          <div className="contact-icon">
+            <Icon
+              src={mail}
+              alt="mail icon"
+              link="mailto:hakan.gundogdu@gmail.com"
+            />
+            <a
+              href="mailto:hakan.gundogdu@gmail.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              hakan.gundogdu@gmail.com
+            </a>
+          </div>
+          <div className="contact-icon">
+            <Icon
+              src={linkedin}
+              alt="linkedin icon"
+              link="https://www.linkedin.com/in/hakangundogdu1/"
+            />
+            <a
+              href="https://www.linkedin.com/in/hakangundogdu1/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              linkedin.com/in/hakangundogdu1/
+            </a>
+          </div>
+          <div className="contact-icon">
+            <Icon
+              src={github}
+              alt="github icon"
+              link="https://github.com/hakangundogdu"
+            />
+            <a
+              href="https://github.com/hakangundogdu"
+              target="_blank"
+              rel="noreferrer"
+            >
+              github.com/hakangundogdu
+            </a>
+          </div>
+          <div className="contact-icon">
+            <Icon
+              src={link}
+              alt="link icon"
+              link="https://upwork.com/freelancers/hakan"
+            />
+            <a
+              href="https://upwork.com/freelancers/hakan"
+              target="_blank"
+              rel="noreferrer"
+            >
+              upwork.com/freelancers/hakan
+            </a>
+          </div>
+          <div className="contact-icon">
+            <Icon
+              src={twitter}
+              alt="twitter icon"
+              link="https://twiter.com/hakancode"
+            />
+            <a
+              href="https://twiter.com/hakancode"
+              target="_blank"
+              rel="noreferrer"
+            >
+              twiter.com/hakancode
+            </a>
+          </div>
         </div>
       </div>
+      <p class="footer-text">
+        &#169; 2021 Hakan Gundogdu. All rigths reserved.
+      </p>
     </div>
   );
 };
